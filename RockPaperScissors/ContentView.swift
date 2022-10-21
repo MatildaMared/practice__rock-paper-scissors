5a3ebcdda528ae8a125ca4adb28799f2ded8d704
Matilda Mared <matildamared@live.se>
2022-10-21 11:36:26 +0200

    add basic outline

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
+enum Choice: String {
+    case rock = "rock"
+    case paper = "paper"
+    case scissors = "scissors"
+}
+
+extension View {
+    func buttonStyles() -> some View {
+        modifier(StyledButton())
+    }
+}
+
+struct StyledButton: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(minWidth: 150)
+            .font(.title2)
+            .foregroundColor(.primary)
+            .padding()
+            .background(.secondary)
+            .foregroundStyle(.ultraThinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
 struct ContentView: View {
+    let choices = [Choice.rock, Choice.paper, Choice.scissors]
+    @State private var currentChoice = Choice.rock
+    @State private var totalScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.yellow, .pink, .purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Rock, Paper, Scissors")
+                    .font(.largeTitle).fontWeight(.light).foregroundStyle(.primary)
+                
+                Button("Rock 🪨") {
+                    
+                }.buttonStyles()
+                
+                Button("Paper 📄") {
+                    
+                }.buttonStyles()
+                
+                Button("Scissors ✂️") {
+                    
+                }.buttonStyles()
+                
+                Text("Total Score: \(totalScore)")
+                    .padding()
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
